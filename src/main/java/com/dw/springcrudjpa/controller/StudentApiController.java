0f6e04a4598145ef2ce4a487381b88025ee75f9f
Tanmak2 <whdcks3131@naver.com>
2022-12-18 23:05:14 +0900

    feat : 학생테이블 전체 조회 완료

--- src/main/java/com/dw/springcrudjpa/controller/StudentApiController.java
+++ src/main/java/com/dw/springcrudjpa/controller/StudentApiController.java
@@ -1,8 +1,22 @@
 package com.dw.springcrudjpa.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dw.springcrudjpa.model.Student;
+import com.dw.springcrudjpa.repository.StudentRepo;
 
 @RestController
 public class StudentApiController {
     
+    @Autowired
+    StudentRepo studentReo;
+
+    @GetMapping("/api/v1/student")
+    public List<Student> callStudents(){
+        return studentReo.findAll();
+    }
 }
